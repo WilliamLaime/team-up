9bb3bd16fa168e253d1b790eaf5c7e7847c1c290
William Laime <laimewilliam@gmail.com>
2026-03-30 14:28:07 +0200

    Fix mailer from address to use MAILER_FROM env var

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,6 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: "from@example.com"
+  # Utilise la variable d'environnement MAILER_FROM (définie sur Heroku)
+  # Cette adresse doit être vérifiée comme "Sender Identity" dans SendGrid
+  default from: ENV.fetch("MAILER_FROM", "noreply@teams-up-sport.fr")
   layout "mailer"
 end
